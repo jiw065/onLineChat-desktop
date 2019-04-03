ca3996919489b81c711d3df5e5aca00530a7f3c0
jiw065 <wwjjjj0923@gmail.com>
2019-04-03 12:26:54 -0400

    ver2.0

--- Online Chatting Practice/src/ChatWindow.java
+++ Online Chatting Practice/src/ChatWindow.java
@@ -1,18 +1,23 @@
+import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.TextArea;
+import java.awt.TextField;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 
 public class ChatWindow extends JFrame {
-	
+	TextField tfTxt = new TextField();
+	TextArea taConten = new TextArea();
 	public void showWindow() {
 		this.setLocation(Constant.WINDOW_X, Constant.WINDOW_Y);
 		this.setSize(Constant.WINDOW_WIDTH, Constant.WINDOW_HEIGHT);
 		this.setTitle("Online Chat Practice");
 		this.setVisible(true);
-		
-	
+		this.add(tfTxt,BorderLayout.SOUTH);
+		this.add(taConten,BorderLayout.NORTH);
+		pack();
 		
 		this.addWindowListener(new WindowAdapter() {
 			@Override
