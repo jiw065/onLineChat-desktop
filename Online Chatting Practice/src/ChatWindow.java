043355351054a657c95d18706ec094905555ced9
jiw065 <wwjjjj0923@gmail.com>
2019-04-03 13:56:16 -0400

    ver3.0
    added textfield actionlistener to print text entered from textfield to
    textarea

--- Online Chatting Practice/src/ChatWindow.java
+++ Online Chatting Practice/src/ChatWindow.java
@@ -2,6 +2,10 @@ import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.TextArea;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -9,22 +13,39 @@ import javax.swing.JFrame;
 
 public class ChatWindow extends JFrame {
 	TextField tfTxt = new TextField();
-	TextArea taConten = new TextArea();
+	TextArea taContent = new TextArea();
+	//action listener will act to all the events so do not use keyaction listener
+	private class TFListiner implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String text = tfTxt.getText();
+			taContent.append(text);
+			taContent.append("\n");
+			tfTxt.setText("");
+			
+		}
+		
+		
+	}
 	public void showWindow() {
 		this.setLocation(Constant.WINDOW_X, Constant.WINDOW_Y);
 		this.setSize(Constant.WINDOW_WIDTH, Constant.WINDOW_HEIGHT);
 		this.setTitle("Online Chat Practice");
 		this.setVisible(true);
 		this.add(tfTxt,BorderLayout.SOUTH);
-		this.add(taConten,BorderLayout.NORTH);
+		this.add(taContent,BorderLayout.NORTH);
 		pack();
 		
+		//exit the application when closing the window
 		this.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
+		taContent.setEditable(false);
+		tfTxt.addActionListener(new TFListiner());
 	}
 	
 
