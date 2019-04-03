aa0488610be68d3d7fb68309b2da485b194e2cfe
jiw065 <wwjjjj0923@gmail.com>
2019-04-03 14:07:16 -0400

    ver 4.0
    coded server side startup

--- Online Chatting Practice/src/ChatServer.java
+++ Online Chatting Practice/src/ChatServer.java
@@ -1,8 +1,21 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ChatServer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			ServerSocket ss = new ServerSocket(6666);
+			while(true) {
+				Socket s = ss.accept();
+				System.out.println("connected to a client"); //delete
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
