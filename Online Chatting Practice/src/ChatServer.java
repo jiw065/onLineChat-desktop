3d88efd71d4530147f9d067e57d013a4931746a8
jiw065 <wwjjjj0923@gmail.com>
2019-04-03 17:18:31 -0400

    v7.0
    handle exceptions for server error
    The EOF error is still not resolved, the server will shutdown once
    client is closed

--- Online Chatting Practice/src/ChatServer.java
+++ Online Chatting Practice/src/ChatServer.java
@@ -1,5 +1,6 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,28 +8,46 @@ import java.net.Socket;
 public class ChatServer {
 
 	public static void main(String[] args) {
+		ServerSocket ss = null;
+		DataInputStream dis = null;
+		DataOutputStream dos = null;
+		Socket s = null;
 		try {
-			ServerSocket ss = new ServerSocket(6666);
-			while(true) {
+			ss = new ServerSocket(6666);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		try {
+
+			while (true) {
 				boolean bConnected = false;
-				Socket s = ss.accept();
+				s = ss.accept();
 				bConnected = true;
-				System.out.println("connected to a client"); //delete
-				DataInputStream dis = new DataInputStream(s.getInputStream());
-				DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-				while(bConnected) {
+				System.out.println("connected to a client"); // delete
+				 dis = new DataInputStream(s.getInputStream());
+				 dos = new DataOutputStream(s.getOutputStream());
+				while (bConnected) {
 					String str = dis.readUTF();
 					System.out.println(str);
 				}
-				dis.close();
-				dos.flush();
-				dos.close();
+				
 			}
+		} catch (EOFException e) {
+			System.out.println("client closed");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			try {
+				dis.close();
+				dos.flush();
+				dos.close();
+				s.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
 		}
-		
 
 	}
 
