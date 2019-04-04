9caf12893d66824bbe51cb38299f657f76547941
jiw065 <wwjjjj0923@gmail.com>
2019-04-03 23:17:46 -0400

    mashibin's structure, which is better than manipulate string

--- Online Chatting Practice/src/ChatClient.java
+++ Online Chatting Practice/src/ChatClient.java
@@ -1,62 +1,37 @@
 import java.awt.BorderLayout;
-import java.awt.Robot;
 import java.awt.TextArea;
 import java.awt.TextField;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
 import javax.swing.JFrame;
 
 public class ChatClient extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2934807066947955781L;
 	TextField tfTxt = new TextField();
 	TextArea taContent = new TextArea();
 	Socket s;
 	DataOutputStream dos;
 	DataInputStream dis;
-
 	// action listener will act to all the events so do not use keyaction listener
-	private class TFListiner extends KeyAdapter {
+	private class TFListiner implements ActionListener {
 
 		@Override
-		public void keyPressed(KeyEvent e) {
-			String text = "";
-			if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-				text = tfTxt.getText();
-			}
-
+		public void actionPerformed(ActionEvent e) {
+			String text = tfTxt.getText();
+			taContent.setText(text);
 			writeToServer(text);
-			taContent.setText(readFromServer());
 			tfTxt.setText("");
-		}
-
-	}
-
-	private class RefreshThread implements Runnable {
-
-		@Override
-		public void run() {
-			while (true) {
-				Robot robot;
-				try {
-					robot = new Robot();
-					robot.keyPress(KeyEvent.VK_F5);
-					Thread.sleep(100);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-
-			}
+			
 		}
 
 	}
@@ -70,49 +45,37 @@ public class ChatClient extends JFrame {
 		this.add(taContent, BorderLayout.NORTH);
 		pack();
 		connectToServer();
-
 		// exit the application when closing the window
 		this.addWindowListener(new WindowAdapter() {
 			@Override
-			public void windowClosing(WindowEvent e) {
+			public void windowClosing(WindowEvent e) {	
 				disConnectToServer();
 				System.exit(0);
 			}
 		});
 		taContent.setEditable(false);
-
-		tfTxt.addKeyListener(new TFListiner());
-		new Thread(new RefreshThread()).start();
-
+		tfTxt.addActionListener(new TFListiner());
 	}
 
 	private void writeToServer(String str) {
+		System.out.println("writing"); //test
 		try {
 			dos.writeUTF(str);
 			dos.flush();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
+		//s.close();
 	}
-
-	private String readFromServer() {
-		String str = "";
-		try {
-			str = dis.readUTF();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return str;
-	}
-
 	private void connectToServer() {
 
 		try {
 			s = new Socket("127.0.0.1", 6666);
 			dos = new DataOutputStream(s.getOutputStream());
 			dis = new DataInputStream(s.getInputStream());
-
+			
 			System.out.println("Connect to Server"); // delete
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -121,14 +84,15 @@ public class ChatClient extends JFrame {
 		}
 
 	}
-
+	
 	private void disConnectToServer() {
 		try {
-			writeToServer("EXIT");
+			dos.writeUTF("exiting");
 			dos.close();
 			dis.close();
 			s.close();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
--- Online Chatting Practice/src/ChatServer.java
+++ Online Chatting Practice/src/ChatServer.java
@@ -1,106 +1,54 @@
-import java.util.List;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class ChatServer {
-	List<String> strList = new ArrayList<String>();
-
-	private class clientThread implements Runnable {
-		private Socket ts = null;
-		private boolean bConnected = false;
-		private DataOutputStream dos = null;
-		private DataInputStream dis = null;
-
-		@Override
-		public void run() {
-			System.out.println("Connected to Client " + ts.getPort());
-			bConnected = true;
-
-			try {
-				dis = new DataInputStream(ts.getInputStream());
-				dos = new DataOutputStream(ts.getOutputStream());
-			} catch (IOException e) {
-				System.out.println("initialize data stream error");
-				e.printStackTrace();
-			}
-
-			try {
-				while (bConnected) {
-					String str = dis.readUTF();
-					if (!str.equals("")) {
-						str = "Client " + ts.getPort() + ": " + str + "\n";
-						strList.add(str);
-					}
-
-					String allStr = "";
-					if (str.equals("EXIT"))
-						break;
-					if (!strList.isEmpty()) {
-						for (String s : strList) {
-							allStr += s;
-						}
-					}
-					if (strList.size() > 100) {
-						strList.clear();
-						allStr = "";
-					}
-					dos.writeUTF(allStr);
-				}
-			} catch (IOException e) {
-				System.out.println("Client " + ts.getPort() + " reading error");
-			} finally {
-				bConnected = false;
-				System.out.println("disconnecting client " + ts.getPort());
-				try {
-					dis.close();
-					dos.flush();
-					dos.close();
-					ts.close();
-
-				} catch (IOException e) {
-					System.out.println("client " + ts.getPort() + "closing error");
-				}
-			}
-
-		}
-
-		private void setThreadSocket(Socket s) {
-			ts = s;
-		}
-
-	}
-
-	private void serverStart() {
-		boolean connected = false;
-		ServerSocket ss = null;
-		try {
-			ss = new ServerSocket(6666);
-			connected = true;
-		} catch (IOException e) {
-			System.out.println("Server connection error");
-			e.printStackTrace();
-		}
-		while (connected) {
-			try {
-				Socket s = ss.accept();
-				clientThread ct = new clientThread();
-				ct.setThreadSocket(s);
-				new Thread(ct).start();
-
-			} catch (IOException e) {
-				System.out.println("Server connect to client error");
-				e.printStackTrace();
-			}
-
-		}
-	}
 
 	public static void main(String[] args) {
-		new ChatServer().serverStart();
+		ServerSocket ss = null;
+		DataInputStream dis = null;
+		DataOutputStream dos = null;
+		Socket s = null;
+		try {
+			ss = new ServerSocket(6666);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		try {
+
+			while (true) {
+				boolean bConnected = false;
+				s = ss.accept();
+				bConnected = true;
+				System.out.println("connected to a client"); // delete
+				 dis = new DataInputStream(s.getInputStream());
+				 dos = new DataOutputStream(s.getOutputStream());
+				while (bConnected) {
+					String str = dis.readUTF();
+					System.out.println(str);
+				}
+				
+			}
+		} catch (EOFException e) {
+			System.out.println("client closed");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				dis.close();
+				dos.flush();
+				dos.close();
+				s.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+
 	}
 
 }
